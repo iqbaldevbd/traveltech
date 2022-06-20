423d187d650e35cd18d0833cb864ee853f9d7037
iqbaldevbd <iqbal487.cse@gmail.com>
2022-06-20 23:53:11 +0600

    Project Installation and CRUD Deployment with authentication System

---
+++ routes/web.php
@@ -0,0 +1,49 @@
+<?php
+
+
+use App\Http\Controllers\PostController;
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
+});
+
+
+
+
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+    
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/show/', [PostController::class,'show'])->name('posts.show');
+Route::get('/create/', [PostController::class,'create'])->name('posts.create');
+Route::post('/store/', [PostController::class,'store'])->name('posts.store');
+Route::get('/edit/{post}', [PostController::class,'edit'])->name('posts.edit');
+Route::delete('/destroy/{post}', [PostController::class,'destroy'])->name('posts.destroy');
+Route::put('/update/{post}', [PostController::class,'update'])->name('posts.update');
+
+
+require __DIR__.'/auth.php';
+
+
+
+
